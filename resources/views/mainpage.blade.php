20ab4280995db64491061b2fcb10de1bb2b652fc
Obolduy <ivanox01@yandex.ru>
2021-10-12 10:59:07 +0300

    Url validation in client side

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -1,31 +1,42 @@
-<p>Главная страныца</p>
+<h1>Главная страныца</h1>
 <form id="form">
     @csrf
     <input type="text" name="link" placeholder="Введите текст ссылки">
     <input type="submit" value="Сгенерировать ссылку">
 </form>
 <script>
-    form.onsubmit = async (e) => {
-    let url = new URL(form.elements.link.value);
+form.onsubmit = async (e) => {
+    let url;
+    
+    try {
+        new URL(form.elements.link.value);
+    } catch (_) {
+        e.preventDefault();
+        alert('Введите корректный url');
+        
+        return false;
+    }
+    
+    url = new URL(form.elements.link.value);
     e.preventDefault();
 
     let data = {
-      protocol: url.protocol,
-      origin: url.origin,
-      host: url.host,
-      hostname: url.hostname,
-      pathname: url.pathname,
-      href: url.href,
-      search: url.search,
-      hash: url.hash,
+        protocol: url.protocol,
+        origin: url.origin,
+        host: url.host,
+        hostname: url.hostname,
+        pathname: url.pathname,
+        href: url.href,
+        search: url.search,
+        hash: url.hash,
     }
 
     let response = await fetch('/addlink', {
-      method: 'POST',
-      headers: {
-        'X-CSRF-TOKEN': form.elements._token.value
-      },
-      body: JSON.stringify(data)
+        method: 'POST',
+        headers: {
+            'X-CSRF-TOKEN': form.elements._token.value
+        },
+        body: JSON.stringify(data)
     });
 };
 </script>
