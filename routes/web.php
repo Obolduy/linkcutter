491874cb0f7773345401484f8f01e29c3c3df635
Obolduy <ivanox01@yandex.ru>
2021-10-13 18:56:08 +0300

    More routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{LinkController, AddLinkController};
+use App\Http\Controllers\{RegistrationController, LoginController, AddLinkController};
 
 Route::view('/', 'mainpage');
 Route::post('/addlink', [AddLinkController::class, 'addLink']);
-Route::get('/login', [LoginController::class, '']);
-Route::get('/registration', [RegistrationController::class, '']);
-Route::get('/logout', [LoginController::class, '']);
+Route::match(['GET', 'POST'], '/login', [LoginController::class, 'login']);
+Route::match(['GET', 'POST'], '/registration', [RegistrationController::class, 'registration']);
+Route::get('/logout', [LoginController::class, 'logout']);
 Route::get('/user', [ShowUserController::class, '']);
 Route::get('/user/mylinks', [ShowUserController::class, '']);
 Route::post('/user/addlink', [AddLinkController::class, '']);
