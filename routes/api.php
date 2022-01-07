dadd17d283b2d13d3615fa067d0f9f12e403e804
Obolduy <ivanox01@yandex.ru>
2022-01-07 22:23:03 +0300

    API routes and some setup

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\{AddLinkController, DeleteLinkController, ApiTokenController};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\LinksList;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/link', function (Request $request) {
+        return ["links" => LinksList::where('user_id', $request->user()->id)->get()];
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/link', [AddLinkController::class, 'apiAddLink']);
+
+    Route::delete('/link/{id}', [DeleteLinkController::class, 'deleteLink']);
 });
+
+Route::get('/link/{id}', function (int $id) {
+    return LinksList::where('id', $id)->first();
+});
+
+Route::post('/registration', [ApiTokenController::class, 'apiGenerateToken']);
