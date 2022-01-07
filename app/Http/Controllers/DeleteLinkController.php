add0b1729ad38dd8f7a1798354595543ed6dac19
Obolduy <ivanox01@yandex.ru>
2022-01-07 22:23:57 +0300

    Delete link via api

--- app/Http/Controllers/DeleteLinkController.php
+++ app/Http/Controllers/DeleteLinkController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\{FullLinks, LinksList};
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class DeleteLinkController extends Controller
 {
-    public static function deleteLink(int $link_id)
+    public static function deleteLink(Request $request, int $link_id)
     {
         $link = LinksList::find($link_id);
         $fullLink = FullLinks::select('*')->where('id_in_list', $link_id)->first();
 
-        if ($link->user_id == Auth::id()) {
+        if ($link->user_id === $request->user()->id) {
             $link->delete();
             $fullLink->delete();
+
+            if ($request->isMethod('DELETE')) {
+                return ["Success" => "The link has been deleted"];
+            }
         }
 
-        if ($_SERVER['REQUEST_METHOD']) {
-            return redirect()->intended();
-        }
+        return redirect()->intended();
     }
 }
